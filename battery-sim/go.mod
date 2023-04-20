cfcdd2c18d4408fa009b234d7e790f011b2aac7b
SofieLouise <sofma18@student.sdu.dk>
2023-04-20 12:45:05 +0200

    Battery in go with Kafka

--- battery-sim/go.mod
+++ battery-sim/go.mod
@@ -3,7 +3,13 @@ module github.com/joachimbulow/pem-energy-balance
 go 1.20
 
 require (
-	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/segmentio/kafka-go v0.4.39 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	rsc.io/quote/v4 v4.0.1 // indirect
 	rsc.io/sampler v1.3.0 // indirect
 )
---
+++ battery-sim/src/broker/redis.go
@@ -0,0 +1,106 @@
+package broker
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type RedisBroker struct {
+	client *redis.Client
+	pubsub *redis.PubSub
+	ctx    context.Context
+}
+
+const (
+	address = "localhost:6379"
+)
+
+func NewRedisBroker() (*RedisBroker, error) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: address,
+	})
+
+	if err := rdb.Ping(context.Background()).Err(); err != nil {
+		return nil, fmt.Errorf("failed to ping Redis server: %w", err)
+	}
+
+	pubsub := rdb.Subscribe(context.Background(), "default")
+
+	return &RedisBroker{
+		client: rdb,
+		pubsub: pubsub,
+		ctx:    context.Background(),
+	}, nil
+}
+
+func (rb *RedisBroker) Connect() error {
+	return nil // Redis connection is established in NewRedisBroker()
+}
+
+func (rb *RedisBroker) Disconnect() error {
+	if err := rb.pubsub.Unsubscribe(rb.ctx, "default"); err != nil {
+		return fmt.Errorf("failed to unsubscribe from topic: %w", err)
+	}
+	if err := rb.client.Close(); err != nil {
+		return fmt.Errorf("failed to close Redis client connection: %w", err)
+	}
+	return nil
+}
+
+func (rb *RedisBroker) Publish(topic string, key string, message string) error {
+	if err := rb.client.Publish(rb.ctx, topic, message).Err(); err != nil {
+		return fmt.Errorf("failed to publish message: %w", err)
+	}
+	return nil
+}
+
+func (rb *RedisBroker) Subscribe(topic string) error {
+	if err := rb.pubsub.Subscribe(rb.ctx, topic); err != nil {
+		return fmt.Errorf("failed to subscribe to topic: %w", err)
+	}
+	return nil
+}
+
+func (rb *RedisBroker) Unsubscribe(topic string) error {
+	if err := rb.pubsub.Unsubscribe(rb.ctx, topic); err != nil {
+		return fmt.Errorf("failed to unsubscribe from topic: %w", err)
+	}
+	return nil
+}
+
+func (rb *RedisBroker) Listen(topic string, handler func(params ...[]byte)) error {
+	ch := rb.pubsub.Channel()
+
+	go func() {
+		for msg := range ch {
+			if msg.Channel != topic {
+				continue
+			}
+
+			var message map[string]interface{}
+			if err := json.Unmarshal([]byte(msg.Payload), &message); err != nil {
+				log.Printf("Failed to unmarshal message: %v", err)
+				continue
+			}
+
+			if _, ok := message["timestamp"]; !ok {
+				message["timestamp"] = time.Now().Unix()
+			}
+
+			bytes, err := json.Marshal(message)
+			if err != nil {
+				log.Printf("Failed to marshal message: %v", err)
+				continue
+			}
+
+			handler(bytes)
+		}
+	}()
+
+	return nil
+}
