034326220719f196798685a7ef2ba356bf806cf2
SofieLouise <sofma18@student.sdu.dk>
2023-04-21 15:14:30 +0200

    Updated battery to use map of requests

--- battery-sim/main.go
+++ battery-sim/main.go
@@ -4,45 +4,6 @@ import (
 	"fmt"
 
 	"github.com/joachimbulow/pem-energy-balance/src"
-	"github.com/joachimbulow/pem-energy-balance/src/broker"
-)
-
-const (
-	PEMRequestsTopic     = "pem_requests"
-	PEMResponsesTopic    = "pem_responses"
-	FrequencyMeasurement = "frequency_measurements"
-	BatteryActionsTopic  = "battery_actions"
-	InertiaMeasurement   = "inertia_measurements"
-)
-
-var (
-	// instance of the broker
-	newBroker broker.Broker
-)
-
-type pemRequest struct {
-	ID          string `json:"id"`
-	RequestType string `json:"requestType"`
-}
-
-type pemResponse struct {
-	ID           string `json:"id"`
-	ResponseType string `json:"responseType"`
-}
-
-const (
-	Charge    = "CHARGE"
-	Discharge = "DISCHARGE"
-)
-
-const (
-	Granted = "GRANTED"
-	Denied  = "DENIED"
-)
-
-const (
-	Redis = "REDIS"
-	Kafka = "KAFKA"
 )
 
 var (
@@ -61,5 +22,5 @@ func initialize() {
 
 func startBattery() {
 	battery := src.NewBattery()
-	fmt.Print("battery created: ", battery.ID)
+	fmt.Print("New battery created: ", battery.ID)
 }
