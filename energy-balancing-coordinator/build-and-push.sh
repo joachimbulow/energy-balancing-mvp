4d6d40f050d13c3d1b2ef6325caa9b8774e60b71
Joachim <joachimbulow@hotmail.dk>
2023-05-06 10:46:01 +0200

    feat: flink image and pipeline

---
+++ energy-balancing-coordinator/build-and-push.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Make sure the script exits if any command fails :d
+set -e
+
+# This process is also documented in Notion :)
+read -p "Enter the image version tag: " VERSION
+
+# 1. Build the Maven project
+echo "Building Maven project..."
+mvn clean package
+
+# 2. Build the Docker image
+echo "Building Docker image..."
+docker build -t coordinator .
+
+# 3. Tag the Docker image with the specified version
+echo "Tagging Docker image with version $VERSION..."
+docker tag coordinator pemmvpregistry.azurecr.io/coordinator:v$VERSION
+
+# 4. Log in to the Azure Container Registry using an access token
+echo "Logging in to Azure Container Registry..."
+TOKEN=$(az acr login --name pemmvpregistry --expose-token --output tsv --query accessToken)
+docker login pemmvpregistry.azurecr.io --username 00000000-0000-0000-0000-000000000000 --password $TOKEN
+
+# 5. Push the Docker image to the Azure Container Registry
+echo "Pushing Docker image to Azure Container Registry..."
+docker push pemmvpregistry.azurecr.io/coordinator:v$VERSION
+
+echo "Done!"
