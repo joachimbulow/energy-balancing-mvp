7a7f86c4d7bc7a7194ae6f3a51f62932eb34ce4b
Joachim <joachimbulow@hotmail.dk>
2023-05-06 17:03:17 +0200

    feat: more deployment scripts

--- battery-sim/build-and-push.sh
+++ battery-sim/build-and-push.sh
@@ -18,26 +18,22 @@ set -e
 # This process is also documented in Notion :)
 read -p "Enter the image version tag: " VERSION
 
-# 1. Build the Maven project
-echo "Building Maven project..."
-mvn clean package
-
-# 2. Build the Docker image
+# 1. Build the Docker image
 echo "Building Docker image..."
-docker build -t coordinator .
+docker build -t battery .
 
-# 3. Tag the Docker image with the specified version
+# 2. Tag the Docker image with the specified version
 echo "Tagging Docker image with version $VERSION..."
-docker tag coordinator pemmvpregistry.azurecr.io/coordinator:v$VERSION
+docker tag battery pemmvpregistry.azurecr.io/battery:v$VERSION
 
-# 4. Log in to the Azure Container Registry using an access token
+# 3. Log in to the Azure Container Registry using an access token
 echo "Logging in to Azure Container Registry..."
 TOKEN=$(az acr login --name pemmvpregistry --expose-token --output tsv --query accessToken)
 docker login pemmvpregistry.azurecr.io --username 00000000-0000-0000-0000-000000000000 --password $TOKEN
 
-# 5. Push the Docker image to the Azure Container Registry
+# 4. Push the Docker image to the Azure Container Registry
 echo "Pushing Docker image to Azure Container Registry..."
-docker push pemmvpregistry.azurecr.io/coordinator:v$VERSION
+docker push pemmvpregistry.azurecr.io/battery:v$VERSION
 
 echo "Done!"
-echo "Your image tag name is: pemmvpregistry.azurecr.io/coordinator:v$VERSION"
+echo "Your image tag name is: pemmvpregistry.azurecr.io/battery:v$VERSION"
